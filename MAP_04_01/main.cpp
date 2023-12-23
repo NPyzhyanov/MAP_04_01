9b2edf23455317fff4123c4129aefd901aeaf02c
NPyzhyanov <pyzhyanov_n@aol.com>
2023-12-22 20:12:13 -0400

    Insignificant ctor parameters removed

--- MAP_04_01/main.cpp
+++ MAP_04_01/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <vector>
 
-using namespace std;
+#include "shape.h"
+#include "transform.h"
 
-int main(int argc, char *argv[])
+int main()
 {
-    cout << "Hello World!" << endl;
+    Shape test_line(0, 5, 3, 99, 8, 7, 99);
+    Shape test_sqr(1, -10, -10, 99, 30, 15, 99);
+    Shape test_cube(2, 8, 7, 6, 10, 10, 10);
+    Shape test_circle(3, -4, 2, 2.52313, 99);
+    Shape test_cilinder(4, 7, 16, 1.78412, 4.5);
+    
+    Shape* Figures[] = {&test_line, &test_sqr, &test_cube, &test_circle, &test_cilinder};
+    
+    std::cout << "Figure\tvolume\tsquare\n";
+    for (unsigned int fig = 0; fig < sizeof(Figures) / sizeof(Figures[0]); fig++)
+    {
+        std::cout << Figures[fig]->getType() << "\t" << Figures[fig]->volume << "\t" << Figures[fig]->square << std::endl;
+    }
+    
     return 0;
 }
--- MAP_04_01/shape.cpp
+++ MAP_04_01/shape.cpp
@@ -1,31 +1,31 @@
 #define _USE_MATH_DEFINES
 #include"shape.h"
 #include<cmath>
-Shape::Shape(int _type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y3, int _z3, int _x4, int _y4, int _z4, int _x5, int _y5, int _z5, int _x6, int _y6, int _z6, int _x7, int _y7, int _z7, int _x8, int _y8, int _z8)
+Shape::Shape(int _type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2)
 {
 	type = _type;
-	// заполн¤ем координаты фигуры
+	// заполняем координаты фигуры
 	switch (type)
 	{
-	case line: 
+	case line:
 		x1 = _x1; y1 = _y1;
 		x2 = _x2; y2 = _y2;
 		break;
 	case sqr:
 		x1 = _x1; y1 = _y1;
 		x2 = _x2; y2 = _y2;
-		x3 = _x3; y3 = _y3;
-		x4 = _x4; y4 = _y4;
+		x3 = _x1; y3 = _y2;
+		x4 = _x2; y4 = _y1;
 		break;
 	case cube:
 		x1 = _x1; y1 = _y1; z1 = _z1;
 		x2 = _x2; y2 = _y2; z2 = _z2;
-		x3 = _x3; y3 = _y3; z3 = _z3;
-		x4 = _x4; y4 = _y4; z4 = _z4;
-		x5 = _x5; y5 = _y5; z5 = _z5;
-		x6 = _x6; y6 = _y6; z6 = _z6; 
-		x7 = _x7; y7 = _y7; z7 = _z7;
-		x8 = _x8; y8 = _y8; z8 = _z8;
+		x3 = _x1; y3 = _y2; z3 = _z2;
+		x4 = _x1; y4 = _y1; z4 = _z2;
+		x5 = _x2; y5 = _y1; z5 = _z2;
+		x6 = _x2; y6 = _y1; z6 = _z1; 
+		x7 = _x2; y7 = _y2; z7 = _z1;
+		x8 = _x1; y8 = _y2; z8 = _z1;
 		break;
 	default:
 		break;
@@ -69,8 +69,9 @@ Shape::Shape(int _type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, in
 
 }
 
-Shape::Shape(int type, int _x1, int _y1, double R, double H)
+Shape::Shape(int _type, int _x1, int _y1, double R, double H)
 {
+    type = _type;
 	// заполн¤ем координаты фигуры
 	switch (type)
 	{
